7080c6ddd4e23c0a8a832e3965675e2157773a04
rohitvahidya631 <141270286+rohitvahidya631@users.noreply.github.com>
2023-08-03 14:06:03 +0530

    stud project done
    17)Student find-all API
        student/get-all
    18)Student update API
        student/edit/{stdId}
    19)Student delete API
        student/del/{stdId}
    20)Student find by id API
        student/{stdId}

--- emp/src/main/java/com/emp/controller/StudentController.java
+++ emp/src/main/java/com/emp/controller/StudentController.java
@@ -1,12 +1,14 @@
 package com.emp.controller;
 
+import com.emp.model.Employee;
 import com.emp.model.Student;
 import com.emp.response.Response;
 import com.emp.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class StudentController {
@@ -18,6 +20,59 @@ public class StudentController {
     public Response<Student> save(@RequestBody Student student){
         Student stud = studentService.save(student);
         Response<Student> response = new Response<>(stud,201,"CREATED");
+        return response;
+    }
+
+    @GetMapping(value = "/stud/get-all")
+        public Response<List<Student>> getAll()
+        {
+            List<Student> listOfStudent = studentService.findAll();
+            Response<List<Student>> response;
+
+            if(ObjectUtils.isEmpty(listOfStudent))
+            {
+                response = new Response<>(listOfStudent,204,"Not Found");
+            }else{
+                response = new Response<>(listOfStudent,200,"Success");
+
+            }
+            return response;
+        }
+
+    @PutMapping(value = "/stud/edit/{studId}")
+    public Response<Student> studUpdate(@RequestBody Student student,@PathVariable("studId") Integer studID){
+        Student stud = studentService.studUpdate(student,studID);
+        Response<Student> response;
+        if(ObjectUtils.isEmpty(stud)){
+            response = new Response<>(null,204,"NOT-FOUND");
+        }else{
+            response = new Response<>(stud,201,"OK");
+        }
+        return response;
+    }
+
+    @DeleteMapping(value = "/stud/del/{studId}")
+    public Response<Boolean> studDel(@PathVariable("studId") Integer studID){
+        Boolean result = studentService.studDelete(studID);
+        Response<Boolean> response;
+        if(result){
+            response = new Response<>(true,200,"OK");
+        }
+        else{
+            response = new Response<>(false,204,"NOT-FOUND");
+        }
+        return response;
+    }
+
+    @GetMapping(value = "/stud/{studId}")
+    public Response<Student> getStud(@PathVariable("studId") Integer studId){
+        Student emp = studentService.findById(studId);
+        Response<Student> response;
+        if(ObjectUtils.isEmpty(emp)){
+            response = new Response<>(emp,204,"NOT-FOUND");
+        }else{
+            response = new Response<>(emp,201,"OK");
+        }
         return response;
     }
 }
--- emp/src/main/java/com/emp/service/StudentService.java
+++ emp/src/main/java/com/emp/service/StudentService.java
@@ -1,9 +1,13 @@
 package com.emp.service;
 
+import com.emp.model.Employee;
 import com.emp.model.Student;
 import com.emp.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -13,5 +17,34 @@ public class StudentService {
 
     public Student save(Student student){
         return studentRepository.save(student);
+    }
+
+    public List<Student> findAll() {
+        return studentRepository.findAll();
+    }
+
+    public Student studUpdate(Student student, Integer studID) {
+        Optional<Student> studentOptional =  studentRepository.findById(studID);
+        student.setId(studID);
+        return studentRepository.save(student); // Save will do save and update
+    }
+
+    public Boolean studDelete(Integer studID){
+        Optional<Student> studentOptional =  studentRepository.findById(studID);
+        if(studentOptional.isPresent()){
+            studentRepository.deleteById(studID);
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public Student findById(Integer studId) {
+        Optional<Student> studentOptional =  studentRepository.findById(studId);
+        if(studentOptional.isPresent()){
+            return studentOptional.get();
+        }
+        return null;
     }
 }
